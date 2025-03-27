7641dd43ac442267b756b6aefb0fcf29055fe3af
KIYE DENDI MOUBARAK <mkiyedendi@gmail.com>
2025-03-27 09:41:56 +0100

    Premier commit

---
+++ src/modeles/Panier.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Panier = sequelize.define('Panier', {
+    id_panier: { 
+        type: DataTypes.INTEGER, 
+        autoIncrement: true, 
+        primaryKey: true 
+    },
+    date_creation: { 
+        type: DataTypes.DATE, 
+        allowNull: false, 
+        defaultValue: DataTypes.NOW 
+    }
+}, {
+    tableName: 'panier',
+    timestamps: false
+});
+
+module.exports = Panier;
